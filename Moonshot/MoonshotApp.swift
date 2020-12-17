27c06a0f225125902e295ab5371acf5f3e8fcd5b
Michele Volpato <mvolpato@users.noreply.github.com>
2020-12-17 19:28:29 +0100

    Project created

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Michele Volpato on 17/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
