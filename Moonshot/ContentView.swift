5adde2351a96cc4bb0b3ef7f8520800f89366f17
Michele Volpato <mvolpato@users.noreply.github.com>
2020-12-17 19:58:48 +0100

    Day 39

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,10 +7,80 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
+struct User: Codable {
+    var name: String
+    var address: Address
+}
+
+struct Address: Codable {
+    var street: String
+    var city: String
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                GeometryReader { geo in
+                    Image("100days")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geo.size.width)
+                }
+                Button("Decode JSON") {
+                    let input = """
+    {
+        "name": "Taylor Swift",
+        "address": {
+            "street": "555, Taylor Swift Avenue",
+            "city": "Nashville"
+        }
+    }
+    """
+
+                    let data = Data(input.utf8)
+                    let decoder = JSONDecoder()
+                    if let user = try? decoder.decode(User.self, from: data) {
+                        print(user.address.street)
+                    }
+                }
+                NavigationLink(destination: listLinks) {
+                    Text("Hello World")
+                }
+                ScrollView(.vertical) {
+                    VStack(spacing: 10) {
+                        ForEach(0..<20) {
+                            CustomText("Item \($0)")
+                                .font(.title)
+                        }
+                        
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .navigationBarTitle("SwiftUI")
+        }
+    }
+
+    var listLinks: some View {
+        List(0..<100) { row in
+            NavigationLink(destination: Text("Detail \(row)")) {
+                Text("Row \(row)")
+            }
+        }
     }
 }
 
