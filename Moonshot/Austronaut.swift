8bdac9c1423a950be2bf3a6162f86e0f2a73b0d7
Michele Volpato <mvolpato@users.noreply.github.com>
2020-12-18 21:56:10 +0100

    Day 40

---
+++ Moonshot/Austronaut.swift
@@ -0,0 +1,12 @@
+//
+//  Austronaut.swift
+//  Moonshot
+//
+//  Created by Michele Volpato on 18/12/2020.
+//
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,81 +7,30 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    var text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
-
-struct User: Codable {
-    var name: String
-    var address: Address
-}
-
-struct Address: Codable {
-    var street: String
-    var city: String
-}
-
 struct ContentView: View {
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
     var body: some View {
         NavigationView {
-            VStack {
-                GeometryReader { geo in
-                    Image("100days")
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail view")) {
+                    Image(mission.image)
                         .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: geo.size.width)
-                }
-                Button("Decode JSON") {
-                    let input = """
-    {
-        "name": "Taylor Swift",
-        "address": {
-            "street": "555, Taylor Swift Avenue",
-            "city": "Nashville"
-        }
-    }
-    """
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
 
-                    let data = Data(input.utf8)
-                    let decoder = JSONDecoder()
-                    if let user = try? decoder.decode(User.self, from: data) {
-                        print(user.address.street)
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
                     }
-                }
-                NavigationLink(destination: listLinks) {
-                    Text("Hello World")
-                }
-                ScrollView(.vertical) {
-                    VStack(spacing: 10) {
-                        ForEach(0..<20) {
-                            CustomText("Item \($0)")
-                                .font(.title)
-                        }
-                        
-                    }
-                    .frame(maxWidth: .infinity)
                 }
             }
-            .navigationBarTitle("SwiftUI")
+            .navigationBarTitle("Moonshot")
         }
     }
 
-    var listLinks: some View {
-        List(0..<100) { row in
-            NavigationLink(destination: Text("Detail \(row)")) {
-                Text("Row \(row)")
-            }
-        }
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
